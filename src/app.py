a5fbeb493f5ba7e39d343132233d08ca251a8a67
Clecio Antao <clecio.antao@gmail.com>
2024-11-23 12:23:43 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"message": "Hello, Humanitec! Realizando POC de deploy com Humanitec - New!!!  KKKKYYYYYZZZZZZZZ  " + current_time})
+    return jsonify({"message": "Hello, Humanitec! Realizando POC de deploy com Humanitec - New!!!  Saturday!!!  " + current_time})
 
 @app.route('/health')
 def health():
